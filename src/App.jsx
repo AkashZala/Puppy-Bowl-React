6f75dc41bb144615a94fb09debe39063bf707529
Akash Zala <akashzala@Akashs-MacBook-Pro.local>
2023-09-12 13:43:35 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Nav from "./Nav"
+
+function App() {
+
+
+  return (
+    <>
+      <h1>Puppy Bowl React </h1>
+      <Nav />
+    </>
+  )
+}
+
+export default App
---
+++ src/Nav.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Nav = () => {
+    return(
+        <div>
+            <Link>Home</Link>
+            <Link>Puppies</Link>
+        </div>
+    );
+}
+
+export default Nav;
